363050ac0b3dc145890b00dc15b56501b00a748a
yamesyung <simionut@gmail.com>
2024-03-29 12:47:27 +0200

    added overlays script into separate file

---
+++ static/js/book/book-gallery.js
@@ -0,0 +1,10 @@
+
+// Function to show the overlay
+function showOverlay() {
+    document.getElementById("overlay").style.display = "block";
+}
+
+
+function closeOverlay() {
+    document.getElementById('overlay').style.display = 'none'; // Hide overlay
+}
