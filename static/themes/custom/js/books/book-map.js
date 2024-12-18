90b740705c2d5f3ab418c8021b285bf5070dd658
john <simionut@gmail.com>
2024-12-18 22:12:08 +0200

    added custom style for map views

--- static/themes/custom/js/books/book-map.js
+++ static/themes/custom/js/books/book-map.js
@@ -4,8 +4,8 @@ console.log(locations_json);
 
 var map = L.map('map').setView([20, 20], 2);
 
-var Esri_WorldGrayCanvas = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
-	attribution: 'Tiles &copy; Esri &mdash; Esri, TomTom, Garmin, FAO, NOAA, USGS',
+var Esri_WorldGrayCanvas = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community',
 	maxZoom: 13,
 
 }).addTo(map);
