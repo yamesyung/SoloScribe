86b3607c6467095fb08127dee056c528ac21398c
john <simionut@gmail.com>
2024-12-25 15:20:07 +0200

    added pikabook skeleton

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import HomePageView, AboutPageView, changelog
+from .views import HomePageView, AboutPageView, changelog, pikabook, replace_cover
 
 urlpatterns = [
     path("about/", AboutPageView.as_view(), name="about"),
@@ -9,6 +9,8 @@ urlpatterns = [
 
 htmx_urlpatterns = [
     path("changelog/", changelog, name="changelog"),
+    path("pikabook/", pikabook, name="pikabook"),
+    path('replace-cover/<int:book_id>/', replace_cover, name='replace_cover'),
 ]
 
 urlpatterns += htmx_urlpatterns
--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+import random
+
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import TemplateView
 
 from accounts.views import get_current_theme
+from books.models import Book
 
 
 class HomePageView(TemplateView):
@@ -19,4 +22,31 @@ class AboutPageView(TemplateView):
 
 
 def changelog(request):
+    """
+    renders a partial containing the  changelog
+    """
     return render(request, 'partials/homepage/changelog.html')
+
+
+def pikabook(request):
+    """
+    start of a homepage minigame where you have to choose between 2 books based on your own criteria.
+    Can add animations, related model for scores or whatever. Hidden for now.
+    """
+    books = Book.objects.filter(review__bookshelves='dnf')[:2]
+    context = {'books': books}
+    return render(request, 'partials/homepage/pikabook.html', context)
+
+
+def replace_cover(request, book_id):
+    """
+    see above
+    """
+    selected_book = get_object_or_404(Book, goodreads_id=book_id)
+    other_books = Book.objects.exclude(goodreads_id=book_id)
+    random_book = random.choice(other_books) if other_books.exists() else None
+
+    books = [selected_book]
+    if random_book:
+        books.append(random_book)
+    return render(request, "partials/homepage/pikabook.html", {"books": books})
