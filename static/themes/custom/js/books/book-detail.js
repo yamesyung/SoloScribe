c4cb041846198d1b0dee5ab7add80fb987a00c0f
john <simionut@gmail.com>
2025-01-17 21:38:52 +0200

    added style for overlay forms

--- static/themes/custom/js/books/book-detail.js
+++ static/themes/custom/js/books/book-detail.js
@@ -1,3 +1,33 @@
+let palette = null;
+
+const backgroundImage = getComputedStyle(document.body).backgroundImage;
+const imageUrl = backgroundImage.slice(5, -2);
+
+const img = new Image();
+img.src = imageUrl;
+
+img.onload = () => {
+  const colorThief = new ColorThief();
+  palette = colorThief.getPalette(img, 2);     // Array of [R, G, B]
+
+  if (palette) {
+    palette.sort((a, b) => {
+      const brightnessA = 0.299 * a[0] + 0.587 * a[1] + 0.114 * a[2];
+      const brightnessB = 0.299 * b[0] + 0.587 * b[1] + 0.114 * b[2];
+      return brightnessA - brightnessB;
+    });
+  }
+  else {
+    palette = [[255, 255, 255], [255, 255, 255]];
+  }
+
+};
+
+img.onerror = () => {
+  console.error('Failed to load image');
+};
+
+
 function showQuoteOverlay() {
     document.getElementById("quote-overlay").style.display = "block";
 }
@@ -58,7 +88,11 @@ document.addEventListener("htmx:afterSwap", function(event) {
             }
 
         }
-
+        const overlay = document.getElementById('quote-overlay');
+        const gradient = `radial-gradient(circle,
+            rgb(${palette[1][0]}, ${palette[1][1]}, ${palette[1][2]}),
+            rgb(${palette[0][0]}, ${palette[0][1]}, ${palette[0][2]}))`;
+        overlay.style.background = gradient;
     }
 });
 
