b8cb4eda91a77a70b473865578d3b774f4febaa1
john <simionut@gmail.com>
2025-01-23 12:21:40 +0200

    added some interactions for quote page

--- static/themes/dark/js/books/quotes.js
+++ static/themes/dark/js/books/quotes.js
@@ -14,3 +14,24 @@ document.addEventListener("htmx:afterSwap", function(event) {
         }, 100);
     }
 });
+
+document.body.addEventListener("htmx:afterSwap", function(event) {
+    // Add event listener for change events on .book-checkbox elements
+    event.detail.elt.querySelectorAll('.quote-fav').forEach(function(checkbox) {
+        checkbox.addEventListener('change', function(event) {
+            var checkboxId = event.target.id;
+            // Get the corresponding book link element
+            var quoteContainer = document.querySelector('#quote-' + checkboxId.split('-')[1]);
+
+            // Check if the book link element exists
+            if (quoteContainer) {
+                // Check if the checkbox is checked and update the class of the book link accordingly
+                if (event.target.checked) {
+                    quoteContainer.classList.add('favorite');
+                } else {
+                    quoteContainer.classList.remove('favorite');
+                }
+            }
+        });
+    });
+});
